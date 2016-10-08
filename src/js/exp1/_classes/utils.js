4ac546268927cc50d79e84f4c95f029e94e1b2fb
Theo <theo.costantini@gmail.com>
2016-10-08 19:33:15 +0200

    [exp1] Add Utils class

---
+++ src/js/exp1/_classes/utils.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let debugMode = false;
+
+class Utils {
+	constructor() {
+
+	}
+
+	static setDebugMode(b) {
+		debugMode = b;
+	}
+
+	static log(msg) {
+		if (debugMode) {
+			console.log(msg)
+		}
+	}
+
+	static extendObject(a, b) {
+	  if (a && b) {
+	    for (var key in b) {
+	      if (!a.hasOwnProperty(key)) a[key] = b[key];
+	    }
+	  }
+	}
+}
+
+export default Utils;
