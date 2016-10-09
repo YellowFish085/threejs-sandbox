26517bac65c7eb2d69ff7481e1d2a258bc1a3676
Theo <theo.costantini@gmail.com>
2016-10-09 16:06:29 +0200

    [exp1] Add camera to scene

--- src/js/exp1/scenes/scene.js
+++ src/js/exp1/scenes/scene.js
@@ -1,16 +1,19 @@
 'use strict';
 
-import Utils from '../_classes/utils';
-
-import IdentifiableObject from '../_classes/identifiableObject';
 import * as THREE from 'three';
+
+import Utils from '../_classes/utils';
+import IdentifiableObject from '../_classes/identifiableObject';
+
+import CameraFactory from '../cameras/cameraFactory';
 
 class Scene extends IdentifiableObject {
 	constructor(datas) {
 		super();
 
-		this._name  = datas.name;
-		this._scene = new THREE.Scene();
+		this._name   = datas.name;
+		this._scene  = new THREE.Scene();
+		this._camera = CameraFactory.create(datas.camera);
 	}
 
 	get id() {
